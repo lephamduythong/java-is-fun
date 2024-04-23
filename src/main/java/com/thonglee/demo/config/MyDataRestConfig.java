8647e58cb20d9a0a684905b2c5b930652009246d
Thong Le <lephamduythong2296@gmail.com>
2024-04-23 15:11:55 +0700

    Not allow POST, PUT, DELETE methods

---
+++ src/main/java/com/thonglee/demo/config/MyDataRestConfig.java
@@ -0,0 +1,28 @@
+package com.thonglee.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.thonglee.demo.entity.Product;
+import com.thonglee.demo.entity.ProductCategory;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		HttpMethod[] notAllowedMethods = { HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE };
+		
+		config.getExposureConfiguration()
+			.forDomainType(Product.class)
+			.withItemExposure((metadata, httpMethods) -> httpMethods.disable(notAllowedMethods))
+			.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(notAllowedMethods));	
+		
+		config.getExposureConfiguration()
+			.forDomainType(ProductCategory.class)
+			.withItemExposure((metadata, httpMethods) -> httpMethods.disable(notAllowedMethods))
+			.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(notAllowedMethods));	
+	}
+}
