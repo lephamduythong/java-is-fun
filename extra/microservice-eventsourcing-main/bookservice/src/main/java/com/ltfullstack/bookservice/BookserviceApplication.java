2450aadfc29b0f88a24f14dc9b4c9a9dd67def0f
Thong Le <lephamduythong2296@gmail.com>
2025-06-22 10:46:21 +0700

    [Microservices]: Clean some files to run first

--- extra/microservice-eventsourcing-main/bookservice/src/main/java/com/ltfullstack/bookservice/BookserviceApplication.java
+++ extra/microservice-eventsourcing-main/bookservice/src/main/java/com/ltfullstack/bookservice/BookserviceApplication.java
@@ -2,12 +2,12 @@ package com.ltfullstack.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 @ComponentScan({"com.ltfullstack.bookservice","com.ltfullstack.commonservice"})
 public class BookserviceApplication {
 	public static void main(String[] args) {
