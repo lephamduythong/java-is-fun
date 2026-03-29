a5de1702ca2daa725e8caa65bb194b4a5beaf196
Thong Le <lephamduythong2296@gmail.com>
2026-03-29 23:01:48 +0700

    [FE-learning]: Using localStorage to get saved data

--- extra/springboot-template/FE/src/app/service/learning-test.service.ts
+++ extra/springboot-template/FE/src/app/service/learning-test.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from "@angular/core";
+import { Injectable, signal, effect } from "@angular/core";
 import { ItemTest } from "../component/learning-test/item-test/item-test.model";
 
 @Injectable({providedIn: "root"})
@@ -6,13 +6,31 @@ export class LearningTestService {
     
     items = signal<ItemTest[]>([]);
 
+    constructor() {
+        this.loadItemsFromLocalStorage();
+    }
+
     addItem(item: ItemTest) {
         console.log('Adding item in service:', item);
         this.items.update(items => [...items, item]);
+        this.syncItemsToLocalStorage();
     }
 
     removeItem(id: number) {
         console.log('Removing item in service with id:', id);
         this.items.update(items => items.filter(item => item.id !== id));
+        this.syncItemsToLocalStorage();
+    }
+
+    syncItemsToLocalStorage() {
+        localStorage.setItem('learningTestItems', JSON.stringify(this.items()));
+    }
+
+    loadItemsFromLocalStorage() {
+        const itemsJson = localStorage.getItem('learningTestItems');
+        if (itemsJson) {
+            const items = JSON.parse(itemsJson) as ItemTest[];
+            this.items.set(items);
+        }
     }
 }
