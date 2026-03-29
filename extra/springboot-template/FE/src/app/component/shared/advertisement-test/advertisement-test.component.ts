3530d0ac68e7053c17bb110d7a2d327d11bc6e01
Thong Le <lephamduythong2296@gmail.com>
2026-03-29 22:19:55 +0700

    [FE-learning]: Pipe example

--- extra/springboot-template/FE/src/app/component/shared/advertisement-test/advertisement-test.component.ts
+++ extra/springboot-template/FE/src/app/component/shared/advertisement-test/advertisement-test.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectorRef, Component, computed, inject, Input, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
     selector: 'app-shared-advertisement-test',
     standalone: true,
-    imports: [FormsModule],
+    imports: [FormsModule, CommonModule, DatePipe],
     templateUrl: './advertisement-test.component.html',
     styleUrls: [
         // './advertisement-test.component.css', 
@@ -13,4 +14,5 @@ import { FormsModule } from '@angular/forms';
 export class SharedAdvertisementTestComponent {
     private cdr = inject(ChangeDetectorRef);
     
+    today = new Date();
 }
