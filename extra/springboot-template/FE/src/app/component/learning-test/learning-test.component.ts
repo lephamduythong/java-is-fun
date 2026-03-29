91b9ef3641d70cede9dc2572155076f4f159e50a
Thong Le <lephamduythong2296@gmail.com>
2026-03-29 17:57:45 +0700

    [FE-learning]: listening event and changing state data

--- extra/springboot-template/FE/src/app/component/learning-test/learning-test.component.ts
+++ extra/springboot-template/FE/src/app/component/learning-test/learning-test.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import Constants from '../../common/js/constants';
+import { generateRandomString } from '../../common/js/utils';
 
 @Component({
     selector: 'app-learning-test',
@@ -14,7 +15,14 @@ import Constants from '../../common/js/constants';
 export class LearningTestComponent {
     private cdr = inject(ChangeDetectorRef);
 
+    textTest1 = 'test1';
+
     get imgPath() {
         return `${Constants.IMG_STATIC_PATH}cat.jpg`;
+    }
+
+    onTest1() {
+        console.log('test1');
+        this.textTest1 = generateRandomString(10);
     }
 }
