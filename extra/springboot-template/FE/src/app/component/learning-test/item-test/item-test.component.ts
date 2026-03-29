569a31ad8fbe208c3138cdf400d139742eaf8b3e
Thong Le <lephamduythong2296@gmail.com>
2026-03-29 18:41:06 +0700

    [FE-learning]: singal input (@Input decorator -> input function)

--- extra/springboot-template/FE/src/app/component/learning-test/item-test/item-test.component.ts
+++ extra/springboot-template/FE/src/app/component/learning-test/item-test/item-test.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input, computed } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import Constants from '../../../common/js/constants';
 
@@ -9,6 +9,9 @@ import Constants from '../../../common/js/constants';
     templateUrl: './item-test.component.html',
 })
 export class ItemTestComponent {
-    @Input({required: true}) id!: number;
-    @Input({required: true}) name!: string;
+    // @Input({required: true}) id!: number; // Input (Uppowercase I) is a function that returns a property decorator function. 
+    // @Input({required: true}) name!: string;
+    id = input.required<number>(); // input() (Lowercase i) is a function that returns a property decorator function.
+    name = input.required<string>(); 
+    computed = computed(() => `${this.id()} - ${this.name()}`);
 }
