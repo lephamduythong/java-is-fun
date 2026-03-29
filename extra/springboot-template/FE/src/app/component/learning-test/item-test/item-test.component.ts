54ee2684f40d6cb9d7ebc056308b97b2e35ae858
Thong Le <lephamduythong2296@gmail.com>
2026-03-29 18:58:59 +0700

    Revert "[FE-learning]: signal output syntax alternative"
    This reverts commit 2815f4592bd8f82220b51e287526a44eb5b0b599.

--- extra/springboot-template/FE/src/app/component/learning-test/item-test/item-test.component.ts
+++ extra/springboot-template/FE/src/app/component/learning-test/item-test/item-test.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, output } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import Constants from '../../../common/js/constants';
 
@@ -11,8 +11,7 @@ import Constants from '../../../common/js/constants';
 export class ItemTestComponent {
     @Input({required: true}) id!: number;
     @Input({required: true}) name!: string;
-    // @Output() itemClicked = new EventEmitter<number>();
-    itemClicked = output<number>();
+    @Output() itemClicked = new EventEmitter<number>();
 
     onItemClick() {
         console.log(`Clicked item with id: ${this.id} and name: ${this.name}`);
