eb7455b3502a3954c673020f86958c7e9a411b57
Thong Le <lephamduythong2296@gmail.com>
2026-04-05 00:28:17 +0700

    [FE]: Content projection with ngProjectAs

--- extra/springboot-template/FE/src/app/component/shared/button-test/button-test.component.ts
+++ extra/springboot-template/FE/src/app/component/shared/button-test/button-test.component.ts
@@ -7,7 +7,8 @@ import { CommonModule } from '@angular/common';
     imports: [CommonModule],
     template: `
         <button class="button is-primary">
-            <span><ng-content select=".my-icon"></ng-content></span> &nbsp;
+            <!-- <span><ng-content select=".my-icon"></ng-content></span> &nbsp; -->
+            <span><ng-content select="my-icon-2"></ng-content></span> &nbsp;
             <span><ng-content></ng-content></span>
         </button>
     `,
