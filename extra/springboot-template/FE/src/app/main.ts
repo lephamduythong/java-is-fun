5788eee54d8fbbdfed308abc47361dbfdfa2d598
Thong Le <lephamduythong2296@gmail.com>
2026-04-11 18:14:06 +0700

    [FE]: Logging and malnipulate header or body with Http Intercepter

--- extra/springboot-template/FE/src/app/main.ts
+++ extra/springboot-template/FE/src/app/main.ts
@@ -7,5 +7,27 @@
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App2Component } from './component/app2.component';
+import { HttpResponse, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { catchError, map, tap, throwError } from 'rxjs';
 
-bootstrapApplication(App2Component).catch(err => console.error(err));
+function loggingHttpInterceptor(req: any, next: any) {
+    const modifiedReq = req.clone({
+        setHeaders: { 'CUSTOM-HEADER': 'CUTE' }
+    });
+    console.log('HTTP Request:', modifiedReq);
+    return next(modifiedReq).pipe(
+        tap((event: any) => {
+            if (event instanceof HttpResponse) {
+                console.log('HTTP Response:', event);
+            }
+        }),
+        catchError((error: any) => {
+            console.error('HTTP Error:', error);
+            return throwError(() => error);
+        })
+    );  
+}
+
+bootstrapApplication(App2Component, {
+    providers: [provideHttpClient(withInterceptors([loggingHttpInterceptor]))],
+}).catch(err => console.error(err));
